04c186a195246fdab7b027f8e4845a76244e7d60
Jose Carlos <josecarloslopezm17@gmail.com>
2023-05-09 22:11:58 -0600

    -Corrección del script.js

--- script.js
+++ script.js
@@ -1,41 +1,47 @@
-//costos
 const costoAddCiudad = 50; //Si la ciudad de origen y la ciudad de destino son diferentes
 const costoAddFinSemana = 100; //Si la fecha de salida es en fin de semana 
 const costoAddHora = 75; //Si la hora de salida es en horas pico
-const costoBase = 200; //Costo base del vuelo
-
 let form = document.querySelector('form');
+
+window.addEventListener('load', ()=>{
+  const today = new Date();
+  document.getElementById('fecha').min = today
+  .toISOString()
+  .slice(0,10);
+})
 
 //Calcular costo del boleto
 form.addEventListener('submit', function(event){
   event.preventDefault()
 
-  let ciudadOrigen = document.getElementById('origen').value;
-  let ciudadDestino = document.getElementById('destino').value;
-  let fechaSalida = new Date(document.getElementById('fecha').value);
-  let horaSalida = document.getElementById('hora').value;
-  let costoTotal = costoBase;
+  const ciudadOrigen = document.getElementById('origen').value;
+  const ciudadDestino = document.getElementById('destino').value;
+  const fechaSalidaString = document.getElementById('fecha').value;
+  const fechaSalida = new Date(fechaSalidaString + "T00:00:00.000-04:00");
+  fechaSalida.setMinutes(fechaSalida.getMinutes() - fechaSalida.getTimezoneOffset() - 360);
+  const horaSalida = document.getElementById('hora').value;
+  let totalCost = 200;
 
   if (ciudadOrigen !== ciudadDestino) {
-    costoTotal += costoAddCiudad;
+    totalCost += costoAddCiudad;
   }
 
-  if (fechaSalida.getUTCDay() === 0 || fechaSalida.getUTCDay() === 5 || fechaSalida.getUTCDay() === 6) {
-    costoTotal += costoAddFinSemana;
+  const weekend = [0, 5, 6]
+  if (weekend.includes(fechaSalida.getDay())) {
+    totalCost += costoAddFinSemana;
   }
 
-  let partesHora = horaSalida.split(':');
-  let hora = parseInt(partesHora[0]); //se obtiene solamente la parte de la hora
-
-  if ((hora >= 7 && hora < 9) || (hora >= 16 && hora < 18)) {
-    costoTotal += costoAddHora;
+  let hour = parseInt(horaSalida.split(':')[0]);
+  if ((hour >= 7 && hour < 9) || (hour >= 16 && hour < 18)) {
+    totalCost += costoAddHora;
   }
 
+  let costElement = document.getElementById('costo');
   if (ciudadOrigen === '' || ciudadDestino === '' || isNaN(fechaSalida) || horaSalida === '') {
-    document.getElementById('costo').textContent = "Fill in all fields.";
-    document.getElementById('costo').style.color = "red"
+    costElement.textContent = "Fill in all fields.";
+    costElement.style.color = "red"
   }else{
-    document.getElementById('costo').textContent = "$" + costoTotal;
-    document.getElementById('costo').style.color = "green"
+    costElement.textContent = "$" + totalCost;
+    costElement.style.color = "green"
   }
 })
